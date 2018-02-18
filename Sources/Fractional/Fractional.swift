2e1ca6d2f9aef8efcd0847aa624c9d031c308a02
Ralf Ebert <info@ralfebert.de>
2018-02-18 12:05:33 +0100

    swiftformat

--- Sources/Fractional/Fractional.swift
+++ Sources/Fractional/Fractional.swift
@@ -4,106 +4,98 @@
 public typealias Fraction = Fractional<Int>
 
 private func gcd<Number: BinaryInteger>(_ lhs: Number, _ rhs: Number) -> Number {
-	var lhs = lhs, rhs = rhs
-	while rhs != 0 { (lhs, rhs) = (rhs, lhs % rhs) }
-	return lhs
+    var lhs = lhs, rhs = rhs
+    while rhs != 0 { (lhs, rhs) = (rhs, lhs % rhs) }
+    return lhs
 }
-	
+
 private func lcm<Number: BinaryInteger>(_ lhs: Number, _ rhs: Number) -> Number {
-	return lhs * rhs / gcd(lhs, rhs)
+    return lhs * rhs / gcd(lhs, rhs)
 }
 
 private func reduce<Number: BinaryInteger>(numerator: Number, denominator: Number) -> (numerator: Number, denominator: Number) {
-	var divisor = gcd(numerator, denominator)
-	if divisor < 0 { divisor *= -1 }
-	guard divisor != 0 else { return (numerator: numerator, denominator: 0) }
-	return (numerator: numerator / divisor, denominator: denominator / divisor)
+    var divisor = gcd(numerator, denominator)
+    if divisor < 0 { divisor *= -1 }
+    guard divisor != 0 else { return (numerator: numerator, denominator: 0) }
+    return (numerator: numerator / divisor, denominator: denominator / divisor)
 }
 
-public struct Fractional<Number: BinaryInteger & Codable> : Codable {
-	/// The numerator of the fraction.
-	public let numerator: Number
-	
-	/// The (always non-negative) denominator of the fraction.
-	public let denominator: Number
-	
-	public init(_ numerator: Number, _ denominator: Number) {
-		var (numerator, denominator) = reduce(numerator: numerator, denominator: denominator)
-		if denominator < 0 { numerator *= -1; denominator *= -1 }
-								
-		self.numerator = numerator
-		self.denominator = denominator
-	}
-    
+public struct Fractional<Number: BinaryInteger & Codable>: Codable {
+    /// The numerator of the fraction.
+    public let numerator: Number
+
+    /// The (always non-negative) denominator of the fraction.
+    public let denominator: Number
+
+    public init(_ numerator: Number, _ denominator: Number) {
+        var (numerator, denominator) = reduce(numerator: numerator, denominator: denominator)
+        if denominator < 0 { numerator *= -1; denominator *= -1 }
+
+        self.numerator = numerator
+        self.denominator = denominator
+    }
+
     /// Create an instance initialized to `value`.
     public init(_ value: Number) {
         self.init(value, 1)
     }
-}	
+}
 
 extension Fractional: Equatable {
-    public static func ==(lhs: Fractional, rhs: Fractional) -> Bool {
+    public static func == (lhs: Fractional, rhs: Fractional) -> Bool {
         return lhs.numerator == rhs.numerator && lhs.denominator == rhs.denominator
     }
 }
 
 extension Fractional: Comparable {
-    
-    public static func <(lhs: Fractional, rhs: Fractional) -> Bool {
+    public static func < (lhs: Fractional, rhs: Fractional) -> Bool {
         guard !lhs.isNaN && !rhs.isNaN else { return false }
         guard lhs.isFinite && rhs.isFinite else { return lhs.numerator < rhs.numerator }
         let (lhsNumerator, rhsNumerator, _) = Fractional.commonDenominator(lhs, rhs)
         return lhsNumerator < rhsNumerator
     }
-    
 }
 
 extension Fractional: Hashable {
-	public var hashValue: Int {
-		return numerator.hashValue ^ denominator.hashValue
-	}
+    public var hashValue: Int {
+        return self.numerator.hashValue ^ self.denominator.hashValue
+    }
 }
 
 extension Fractional {
-    
     fileprivate static func commonDenominator(_ lhs: Fractional, _ rhs: Fractional) -> (lhsNumerator: Number, rhsNumberator: Number, denominator: Number) {
         let denominator = lcm(lhs.denominator, rhs.denominator)
         let lhsNumerator = lhs.numerator * (denominator / lhs.denominator)
         let rhsNumerator = rhs.numerator * (denominator / rhs.denominator)
-        
+
         return (lhsNumerator, rhsNumerator, denominator)
     }
-    
 }
 
 extension Fractional: Strideable {
-
     public typealias Stride = Fractional
 
-	public func advanced(by n: Stride) -> Fractional {
-		let (selfNumerator, nNumerator, commonDenominator) = Fractional.commonDenominator(self, n)
+    public func advanced(by n: Stride) -> Fractional {
+        let (selfNumerator, nNumerator, commonDenominator) = Fractional.commonDenominator(self, n)
         return Fractional(selfNumerator + nNumerator, commonDenominator)
-	}
-	
-	public func distance(to other: Fractional) -> Stride {
-		return other.advanced(by: -self)
-	}
+    }
+
+    public func distance(to other: Fractional) -> Stride {
+        return other.advanced(by: -self)
+    }
 }
 
-extension Fractional : ExpressibleByIntegerLiteral {
-    
+extension Fractional: ExpressibleByIntegerLiteral {
     public init(integerLiteral value: IntegerLiteralType) {
         self.init(value as! Number)
     }
-    
 }
 
 extension Fractional: Numeric {
-    
-    public init?<T>(exactly source: T) where T : BinaryInteger {
+    public init?<T>(exactly source: T) where T: BinaryInteger {
         self.init(source as! Number)
     }
-    
+
     public var magnitude: Fractional<UInt> {
         var n = self.numerator
         let d = self.denominator
@@ -112,132 +104,125 @@ extension Fractional: Numeric {
         }
         return Fractional<UInt>(UInt(n), UInt(d))
     }
-    
+
     /// Add `lhs` and `rhs`, returning a reduced result.
-    public static func +(lhs: Fractional<Number>, rhs: Fractional<Number>) -> Fractional<Number> {
+    public static func + (lhs: Fractional<Number>, rhs: Fractional<Number>) -> Fractional<Number> {
         guard !lhs.isNaN && !rhs.isNaN else { return .NaN }
         guard lhs.isFinite && rhs.isFinite else {
             switch (lhs >= 0, rhs >= 0) {
-            case (false, false): return -.infinity
-            case (true, true):   return .infinity
-            default:             return .NaN
+                case (false, false): return -.infinity
+                case (true, true): return .infinity
+                default: return .NaN
             }
         }
-        
+
         let (lhsNumerator, rhsNumerator, commonDenominator) = Fractional.commonDenominator(lhs, rhs)
         return Fractional(lhsNumerator + rhsNumerator, commonDenominator)
     }
-    
-    public static func +=(lhs: inout Fractional<Number>, rhs: Fractional<Number>) {
+
+    public static func += (lhs: inout Fractional<Number>, rhs: Fractional<Number>) {
         lhs = lhs + rhs
     }
-    
+
     /// Subtract `lhs` and `rhs`, returning a reduced result.
-    public static func -(lhs: Fractional<Number>, rhs: Fractional<Number>) -> Fractional<Number> {
+    public static func - (lhs: Fractional<Number>, rhs: Fractional<Number>) -> Fractional<Number> {
         return lhs + -rhs
     }
-    public static func -=(lhs: inout Fractional<Number>, rhs: Fractional<Number>) {
+
+    public static func -= (lhs: inout Fractional<Number>, rhs: Fractional<Number>) {
         lhs = lhs - rhs
     }
-    
+
     /// Multiply `lhs` and `rhs`, returning a reduced result.
-    public static func *(lhs: Fractional<Number>, rhs: Fractional<Number>) -> Fractional<Number> {
+    public static func * (lhs: Fractional<Number>, rhs: Fractional<Number>) -> Fractional<Number> {
         let swapped = (Fractional(lhs.numerator, rhs.denominator), Fractional(rhs.numerator, lhs.denominator))
         return Fractional(swapped.0.numerator * swapped.1.numerator, swapped.0.denominator * swapped.1.denominator)
     }
-    
-    public static func *=<Number>(lhs: inout Fractional<Number>, rhs: Fractional<Number>) {
+
+    public static func *= <Number>(lhs: inout Fractional<Number>, rhs: Fractional<Number>) {
         lhs = lhs * rhs
     }
-    
+
     /// Divide `lhs` and `rhs`, returning a reduced result.
-    public static func /(lhs: Fractional<Number>, rhs: Fractional<Number>) -> Fractional<Number> {
+    public static func / (lhs: Fractional<Number>, rhs: Fractional<Number>) -> Fractional<Number> {
         return lhs * rhs.reciprocal
     }
 
-    public static func /=(lhs: inout Fractional<Number>, rhs: Fractional<Number>) {
+    public static func /= (lhs: inout Fractional<Number>, rhs: Fractional<Number>) {
         lhs = lhs / rhs
     }
 }
 
 extension Fractional: SignedNumeric {
-
     public mutating func negate() {
         self = Fractional(-1 * self.numerator, self.denominator)
     }
-    
 }
 
-
 extension Fractional {
-	/// The reciprocal of the fraction.
-	public var reciprocal: Fractional {
-		get {
-            return Fractional(denominator, numerator)
-		}
-	}
-	
-	/// `true` iff `self` is neither infinite nor NaN
-	public var isFinite: Bool {
-		return denominator != 0 
-	}
-	
-	/// `true` iff the numerator is zero and the denominator is nonzero 
-	public var isInfinite: Bool {
-		return denominator == 0 && numerator != 0
-	}
-	
-	/// `true` iff both the numerator and the denominator are zero
-	public var isNaN: Bool {
-		return denominator == 0 && numerator == 0
-	}
-	
-	/// The positive infinity.
-	public static var infinity: Fractional {
+    /// The reciprocal of the fraction.
+    public var reciprocal: Fractional {
+        return Fractional(self.denominator, self.numerator)
+    }
+
+    /// `true` iff `self` is neither infinite nor NaN
+    public var isFinite: Bool {
+        return self.denominator != 0
+    }
+
+    /// `true` iff the numerator is zero and the denominator is nonzero
+    public var isInfinite: Bool {
+        return self.denominator == 0 && self.numerator != 0
+    }
+
+    /// `true` iff both the numerator and the denominator are zero
+    public var isNaN: Bool {
+        return self.denominator == 0 && self.numerator == 0
+    }
+
+    /// The positive infinity.
+    public static var infinity: Fractional {
         return Fractional(1, 0)
-	}
-	
-	/// Not a number.
-	public static var NaN: Fractional {
+    }
+
+    /// Not a number.
+    public static var NaN: Fractional {
         return Fractional(0, 0)
-	}
+    }
 }
 
 extension Fractional: CustomStringConvertible {
-	public var description: String {
-		guard !isNaN else { return "NaN" }
-		guard !isInfinite else { return (self >= 0 ? "+" : "-") + "Inf" }
-		
-		switch denominator {
-		case 1: return "\(numerator)"
-		default: return "\(numerator)/\(denominator)"
-		}
-	}
+    public var description: String {
+        guard !isNaN else { return "NaN" }
+        guard !isInfinite else { return (self >= 0 ? "+" : "-") + "Inf" }
+
+        switch self.denominator {
+            case 1: return "\(self.numerator)"
+            default: return "\(self.numerator)/\(self.denominator)"
+        }
+    }
 }
 
 extension Fractional where Number == Int {
-    
     public func wholeQuotient(dividingBy other: Fraction) -> Int {
         return Int(Float(self / other))
     }
-    
+
     public func remainder(dividingBy other: Fraction) -> Fraction {
         return self - Fraction(self.wholeQuotient(dividingBy: other)) * other
     }
-
 }
 
 extension Double {
-	/// Create an instance initialized to `value`.
-	public init(_ value: Fraction) {
-		self.init(Double(value.numerator) / Double(value.denominator))
-	}
+    /// Create an instance initialized to `value`.
+    public init(_ value: Fraction) {
+        self.init(Double(value.numerator) / Double(value.denominator))
+    }
 }
 
 extension Float {
-	/// Create an instance initialized to `value`.
-	public init(_ value: Fraction) {
-		self.init(Float(value.numerator) / Float(value.denominator))
-	}
+    /// Create an instance initialized to `value`.
+    public init(_ value: Fraction) {
+        self.init(Float(value.numerator) / Float(value.denominator))
+    }
 }
-
--- Tests/FractionalTests/FractionalTests.swift
+++ Tests/FractionalTests/FractionalTests.swift
@@ -1,38 +1,37 @@
 // © 2015-2018 Jaden Geller, Brian Tiger Chow, Ralf Ebert
 // License: https://opensource.org/licenses/MIT
 
-import XCTest
 import Fractional
+import XCTest
 
 class FractionalTests: XCTestCase {
-    
     func testReduce() {
         let f68 = Fraction(6, 8)
         XCTAssertEqual(3, f68.numerator)
         XCTAssertEqual(4, f68.denominator)
         XCTAssertEqual(Fraction(3, 4), f68)
     }
-    
+
     func testNegativeDenominator() {
         let f68 = Fraction(-3, -4)
         XCTAssertEqual(3, f68.numerator)
         XCTAssertEqual(4, f68.denominator)
     }
-    
+
     func testLiteral() {
         XCTAssertEqual(Fraction(3, 1), 3 as Fraction)
     }
-    
+
     func testReciprocal() {
         XCTAssertEqual(Fraction(4, 3), Fraction(3, 4).reciprocal)
     }
-    
+
     func testDivision() {
         XCTAssertEqual(
             Fraction(1, 4),
             Fraction(1, 8) / Fraction(1, 2))
     }
-    
+
     func testMultiply() {
         XCTAssertEqual(
             Fraction(1, 16),
@@ -40,107 +39,102 @@ class FractionalTests: XCTestCase {
     }
 
     func testComparable() {
-        XCTAssertEqual(3/4 as Fraction, 3/4 as Fraction)
-        XCTAssertTrue((5/4 as Fraction) > (3/4 as Fraction))
-        XCTAssertTrue((2/4 as Fraction) < (3/4 as Fraction))
+        XCTAssertEqual(3 / 4 as Fraction, 3 / 4 as Fraction)
+        XCTAssertTrue((5 / 4 as Fraction) > (3 / 4 as Fraction))
+        XCTAssertTrue((2 / 4 as Fraction) < (3 / 4 as Fraction))
     }
-    
+
     func testRange() {
-        let range = (2/4 as Fraction)..<(5/4 as Fraction)
-        
-        XCTAssertFalse(range.contains(1/4 as Fraction))
-        XCTAssertTrue(range.contains(2/4 as Fraction))
-        XCTAssertTrue(range.contains(3/4 as Fraction))
-        XCTAssertFalse(range.contains(5/4 as Fraction))
+        let range = (2 / 4 as Fraction) ..< (5 / 4 as Fraction)
+
+        XCTAssertFalse(range.contains(1 / 4 as Fraction))
+        XCTAssertTrue(range.contains(2 / 4 as Fraction))
+        XCTAssertTrue(range.contains(3 / 4 as Fraction))
+        XCTAssertFalse(range.contains(5 / 4 as Fraction))
     }
-    
+
     func testStride() {
-        
-        XCTAssertEqual([1/4 as Fraction, 1/2 as Fraction, 3/4 as Fraction, 1 as Fraction], Array(stride(from: 1/4 as Fraction, to: 5/4 as Fraction, by: 1/4 as Fraction)))
-        
+        XCTAssertEqual([1 / 4 as Fraction, 1 / 2 as Fraction, 3 / 4 as Fraction, 1 as Fraction], Array(stride(from: 1 / 4 as Fraction, to: 5 / 4 as Fraction, by: 1 / 4 as Fraction)))
     }
-    
+
     func testDescription() {
         XCTAssertEqual("4", String(describing: 4 as Fraction))
-        XCTAssertEqual("1/2", String(describing: 2/4 as Fraction))
+        XCTAssertEqual("1/2", String(describing: 2 / 4 as Fraction))
     }
-    
+
     func testMagnitude() {
-        
         let f = Fractional<Int>(-3, 4)
-        let fu : Fractional<UInt> = f.magnitude
-        
+        let fu: Fractional<UInt> = f.magnitude
+
         XCTAssertEqual(fu, Fractional<UInt>(3, 4))
-        
     }
 
     func testNaN() {
-        XCTAssertEqual(Fraction.NaN, 0/0 as Fraction)
-        XCTAssertEqual(Fraction.NaN, -1/0 + 1/0 as Fraction)
-        XCTAssertEqual(Fraction.NaN, 1/0 + -1/0 as Fraction)
-        XCTAssertEqual(Fraction.NaN, 1/0 + .NaN as Fraction)
-        XCTAssertEqual(Fraction.NaN, 1/0 * .NaN as Fraction)
+        XCTAssertEqual(Fraction.NaN, 0 / 0 as Fraction)
+        XCTAssertEqual(Fraction.NaN, -1 / 0 + 1 / 0 as Fraction)
+        XCTAssertEqual(Fraction.NaN, 1 / 0 + -1 / 0 as Fraction)
+        XCTAssertEqual(Fraction.NaN, 1 / 0 + .NaN as Fraction)
+        XCTAssertEqual(Fraction.NaN, 1 / 0 * .NaN as Fraction)
     }
-    
-    func testInfinity() {
-        XCTAssertEqual(Fraction.infinity, 1/0 as Fraction)
-        XCTAssertEqual(-Fraction.infinity, -1/0 as Fraction)
-        XCTAssertEqual(Fraction.infinity, 1/0 + 1/0 as Fraction)
-        XCTAssertEqual(-Fraction.infinity, -1/0 + -1/0 as Fraction)
-        XCTAssertEqual(Fraction.infinity, 1/0 * 1/0 as Fraction)
-        XCTAssertEqual(-Fraction.infinity, -1/0 * 1/0 as Fraction)
-        XCTAssertEqual(Fraction.infinity, -1/0 * -1/0 as Fraction)
-    }
-    
-    func testMath() {
-        XCTAssertEqual(3/4 as Fraction, 1/2 + 1/4 as Fraction)
-        XCTAssertEqual(1 as Fraction, 3/4 * (3/4 as Fraction).reciprocal)
 
-        XCTAssertEqual(5/2 as Fraction, 1/4 * 10 as Fraction)
-        XCTAssertEqual(1/2 as Fraction, (1/4) / (1/2) as Fraction)
+    func testInfinity() {
+        XCTAssertEqual(Fraction.infinity, 1 / 0 as Fraction)
+        XCTAssertEqual(-Fraction.infinity, -1 / 0 as Fraction)
+        XCTAssertEqual(Fraction.infinity, 1 / 0 + 1 / 0 as Fraction)
+        XCTAssertEqual(-Fraction.infinity, -1 / 0 + -1 / 0 as Fraction)
+        XCTAssertEqual(Fraction.infinity, 1 / 0 * 1 / 0 as Fraction)
+        XCTAssertEqual(-Fraction.infinity, -1 / 0 * 1 / 0 as Fraction)
+        XCTAssertEqual(Fraction.infinity, -1 / 0 * -1 / 0 as Fraction)
     }
-    
+
+    func testMath() {
+        XCTAssertEqual(3 / 4 as Fraction, 1 / 2 + 1 / 4 as Fraction)
+        XCTAssertEqual(1 as Fraction, 3 / 4 * (3 / 4 as Fraction).reciprocal)
+
+        XCTAssertEqual(5 / 2 as Fraction, 1 / 4 * 10 as Fraction)
+        XCTAssertEqual(1 / 2 as Fraction, (1 / 4) / (1 / 2) as Fraction)
+    }
+
     func testPow() {
         func pow(_ base: Fraction, _ exponent: Int) -> Fraction {
             var result: Fraction = 1
-            for _ in 1...abs(exponent) {
+            for _ in 1 ... abs(exponent) {
                 result *= base
             }
             return exponent >= 0 ? result : result.reciprocal
         }
-        
-        XCTAssertEqual(1/8 as Fraction, pow(1/2, 3))
-        XCTAssertEqual(9 as Fraction, pow(1/3, -2))
+
+        XCTAssertEqual(1 / 8 as Fraction, pow(1 / 2, 3))
+        XCTAssertEqual(9 as Fraction, pow(1 / 3, -2))
     }
 
     func testDivideModulo() {
-        let ref = (5/6 as Fraction)
-        
+        let ref = (5 / 6 as Fraction)
+
         XCTAssertEqual((0 as Fraction).wholeQuotient(dividingBy: ref), 0)
         XCTAssertEqual((0 as Fraction).remainder(dividingBy: ref), 0)
-        
+
         XCTAssertEqual(ref.wholeQuotient(dividingBy: ref), 1)
         XCTAssertEqual(ref.remainder(dividingBy: ref), 0)
-        
-        XCTAssertEqual((3/6 as Fraction).wholeQuotient(dividingBy: ref), 0)
-        XCTAssertEqual((3/6 as Fraction).remainder(dividingBy: ref), 3/6 as Fraction)
-        
-        XCTAssertEqual((13/6 as Fraction).wholeQuotient(dividingBy: ref), 2)
-        XCTAssertEqual((13/6 as Fraction).remainder(dividingBy: ref), 3/6 as Fraction)
-        
-        XCTAssertEqual((-13/6 as Fraction).wholeQuotient(dividingBy: ref), -2)
-        XCTAssertEqual((-13/6 as Fraction).remainder(dividingBy: ref), -3/6 as Fraction)
+
+        XCTAssertEqual((3 / 6 as Fraction).wholeQuotient(dividingBy: ref), 0)
+        XCTAssertEqual((3 / 6 as Fraction).remainder(dividingBy: ref), 3 / 6 as Fraction)
+
+        XCTAssertEqual((13 / 6 as Fraction).wholeQuotient(dividingBy: ref), 2)
+        XCTAssertEqual((13 / 6 as Fraction).remainder(dividingBy: ref), 3 / 6 as Fraction)
+
+        XCTAssertEqual((-13 / 6 as Fraction).wholeQuotient(dividingBy: ref), -2)
+        XCTAssertEqual((-13 / 6 as Fraction).remainder(dividingBy: ref), -3 / 6 as Fraction)
     }
 
     func testToFloatingPoint() {
-        XCTAssertEqual(0.25, Float(1/4 as Fraction))
-        XCTAssertEqual(1/3, Double(1/3 as Fraction))
+        XCTAssertEqual(0.25, Float(1 / 4 as Fraction))
+        XCTAssertEqual(1 / 3, Double(1 / 3 as Fraction))
     }
-    
+
     func testCodeable() throws {
         let str = "{\"numerator\":3,\"denominator\":4}"
-        XCTAssertEqual(str, String(data: try JSONEncoder().encode(3/4 as Fraction), encoding: .utf8))
-        XCTAssertEqual(3/4 as Fraction, try JSONDecoder().decode(Fraction.self, from: str.data(using: .utf8)!))
+        XCTAssertEqual(str, String(data: try JSONEncoder().encode(3 / 4 as Fraction), encoding: .utf8))
+        XCTAssertEqual(3 / 4 as Fraction, try JSONDecoder().decode(Fraction.self, from: str.data(using: .utf8)!))
     }
-    
 }
